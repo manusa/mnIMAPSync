b08cf84fbf67166011058c71116bf7015d0f5694
Marc Nuri <marc@marcnuri.com>
2019-08-20 07:29:28 +0200

    #12: Update Apache 2.0 license headers

--- src/main/java/com/marcnuri/mnimapsync/store/MessageId.java
+++ src/main/java/com/marcnuri/mnimapsync/store/MessageId.java
@@ -1,16 +1,18 @@
-/**
- * Licensed to the Apache Software Foundation (ASF) under one or more contributor license
- * agreements. See the NOTICE file distributed with this work for additional information regarding
- * copyright ownership. The ASF licenses this file to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance with the License. You may obtain a
- * copy of the License at
+/*
+ * Copyright 2013 Marc Nuri San Felix
  *
- * http://www.apache.org/licenses/LICENSE-2.0
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
- * Unless required by applicable law or agreed to in writing, software distributed under the License
- * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
- * or implied. See the License for the specific language governing permissions and limitations under
- * the License.
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
  */
 package com.marcnuri.mnimapsync.store;
 
@@ -58,7 +60,7 @@ public class MessageId implements Serializable {
      * command. The problem is that ENVELOPE is not consistent amongst different servers, so
      * sometimes a same e-mail will have different envelope responses in different servers, so they
      * will duplicate.
-     * 
+     *
      * It's a pity because fetching all of the HEADERS is a performance HOG
      *
      * @param message
